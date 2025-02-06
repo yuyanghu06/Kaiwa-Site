0c257a29c1277759927a68840a56a72ed9244e41
Yuyang Hu <yh5525@nyu.edu>
2025-02-06 16:01:27 -0500

    basic website layout done

---
+++ accounts/account_scripts/account_class.js
@@ -0,0 +1,12 @@
+/*
+This is an implementation of an account object
+@author Yuyang Hu
+@author Isaac Zhang
+*/
+class Account {
+    constructor(email, username, password){
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+}
